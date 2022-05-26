8c49f0b7c293dc054ef6b4f36f41c7f75922b562
Vakurin <vakurin.maxim@gmail.com>
2022-05-26 17:15:33 +0300

    all elements

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-const element = <h1>Hello World</h1>
+// import React from "react"
+// import ReactDOM from "react-dom"
 
-ReactDOM.render(
-    element,
-    document.getElementById("root")
-)
+function SocialMedia() {
+    return (
+        <div>
+            <button className="social-btn">Discord</button>
+            <button className="social-btn">Twitter</button>
+        </div>
+    )
+}
+
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>New Connection</h1>
+                <SocialMedia />
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>contact@newconnection.xyz</small>
+        </footer>
+    )
+}
+
+
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Multichain DAO voting in two clicks</h1>
+            <button>Comming Soon ✨</button>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
