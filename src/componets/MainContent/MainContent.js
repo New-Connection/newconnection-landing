7d48931983840eb7b86e293ba7b2f828147a80d9
Vakurin <vakurin.maxim@gmail.com>
2022-05-28 11:37:48 +0300

    Create componets for footer and main

---
+++ src/componets/MainContent/MainContent.js
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import './MainContent.css';
+
+function MainContent() {
+    return (
+        <div className="main-content">
+            <h1>Multichain<br />DAO voting<br />in two clicks</h1>
+            <button>Comming Soon ✨</button>
+        </div>
+    )
+}
+
+export default MainContent;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import { React }  from 'react';
 import ReactDOM from "react-dom";
-import { Header }  from './componets/';
+import { Header, Footer, MainContent }  from './componets/';
 import './index.css';
-
-function Footer() {
-    return (
-        <footer className="footer">
-            contact@newconnection.xyz
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div className="main-content">
-            <h1>Multichain<br />DAO voting<br />in two clicks</h1>
-            <button>Comming Soon ✨</button>
-        </div>
-    )
-}
 
 function Page() {
     return (
