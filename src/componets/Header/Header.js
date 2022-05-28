e13eda9e3f4b0872d668b2adae4dc64b00b28dbd
Vakurin <vakurin.maxim@gmail.com>
2022-05-28 13:50:22 +0300

    Mobile Desing

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -3,12 +3,18 @@ import './Header.css';
 import logo from '../../assets/logo.png';
 import twitter_img from '../../assets/twitter.png'
 import discord_img from '../../assets/discord.png'
+import discord_not_ready from '../../assets/discord-not-ready.png'
+
+const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+};
+const TwitterURL = 'https://twitter.com/NewConnectionX'
 
 function SocialMedia() {
     return (
         <div className='btns'>
-            <button className="social-btn"><img src={discord_img}/></button>
-            <button className="social-btn"><img src={twitter_img}/></button>
+            <button onClick={()=> openInNewTab(TwitterURL)} className="social-btn"><img src={twitter_img}/></button>
+            <button className="social-btn btn-not-ready"><img src={discord_not_ready}/></button>
         </div>
     )
 }
