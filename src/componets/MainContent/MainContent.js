c3c7758c158f01c2e17d98460b0ef3e80eb6a917
Vakurin <vakurin.maxim@gmail.com>
2022-08-07 17:36:25 +0300

    enter to app

--- src/componets/MainContent/MainContent.js
+++ src/componets/MainContent/MainContent.js
@@ -2,11 +2,17 @@ import { React } from 'react'
 import './MainContent.css';
 import TextAnimation from "./TextAnimation";
 
+const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+};
+
+const NCAPP = 'https://app.newconnection.xyz/'
+
 function MainContent() {
     return (
         <div className="main-content">
             <h1>MULTICHAIN DAO<br />SOLUTION TO CREATE<br /><span className="text-transition"><TextAnimation /></span></h1>
-            <button>Comming Soon ✨</button>    
+            <button onClick={()=> openInNewTab(NCAPP)}>Enter App ✨</button>    
         </div>
     )
 }
