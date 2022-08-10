a72fce37c61bf320f3bcc922e131f5b665001edc
Vakurin <vakurin.maxim@gmail.com>
2022-08-10 05:39:07 +0300

    new svg icons

---
+++ src/components/Icons/ExternalLinkIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const ExternalLinkIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-9 w-9"
+            fill="none"
+            viewBox="0 0 30 16"
+            stroke="currentColor"
+            strokeWidth={2}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m 20 4 M 14 4 h 6 m 0 0 v 6 m 0 -6 L 10 14"
+            />
+        </svg>
+    );
+};
