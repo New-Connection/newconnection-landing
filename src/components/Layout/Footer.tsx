9cd896cf14e4c8be47d9476213067bed8ccbca0b
Vakurin <vakurin.maxim@gmail.com>
2022-08-10 05:40:55 +0300

    updated footer with new svg files

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,37 +1,30 @@
 import * as React from "react";
-// import { Separator } from "ariakit/separator";
+import { Separator } from "ariakit/separator";
 
-// import Discord from "assets/discord.png";
-// import Twitter from "assets/twitter.png";
-
+import { TwitterIcon } from "components/Icons/TwitterIcon";
+import { DiscordIcon } from "components/Icons/DiscordIcon";
+// TODO: Need to create footer for mobile version
 const Footer = () => {
-    const SpaceBetweenButtons = () => {
-        return <div className="w-4 h-auto inline-block"></div>;
-    };
-
     return (
         <footer className="absolute bottom-0 w-full h-16">
-            {/* <Separator
+            <Separator
                 orientation="horizontal"
                 className="mt-16 mb-6 h-0 w-full border-t-2 border-purple"
-            /> */}
+            />
             <div className="flex justify-between">
                 <p className="align-middle pt-4 pl-2">contact@newconnection.xyz</p>
-                <div className="pt-4">
+                <div className="pt-4 space-x-4">
                     <button className="cursor-not-allowed">Terms</button>
-                    <SpaceBetweenButtons />
                     <button className="cursor-not-allowed">Privacy</button>
                 </div>
-                <div className="pr-4 pb-2">
-                    <button className="cursor-not-allowed h-16 w-16 bg-gray rounded-md">
-                        {/* <img src={Discord} /> */}
-                        Discord
+                <div className="pb-4 pr-4 space-x-4">
+                    <button className="cursor-not-allowed px-[0.65em] py-[0.65em] bg-gray rounded-md fill-purple hover:fill-gray2">
+                        <DiscordIcon />
                     </button>
-                    <SpaceBetweenButtons />
+
                     <a href="https://twitter.com/NewConnectionX" target="_blank" rel="noreferrer">
-                        <button className="h-16 w-16 bg-gray rounded-md">
-                            {/* <img src={Twitter} /> */}
-                            Twitter
+                        <button className="px-[0.65em] py-[0.65em] bg-gray rounded-md fill-[#1DA1F2] hover:bg-purple hover:fill-white">
+                            <TwitterIcon />
                         </button>
                     </a>
                 </div>
