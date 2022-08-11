87ee074647eccac2c879a8be26075731534e3e3e
Vakurin <vakurin.maxim@gmail.com>
2022-08-11 16:15:34 +0300

    responsive main content

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,21 +3,19 @@ import { ExternalLinkIcon } from "../Icons/ExternalLinkIcon";
 import TextAnimation from "./TextAnimation";
 import MainImg from "assets/main-img.png";
 
-const openInNewTab = (url) => {
-    window.open(url, "_blank", "noopener,noreferrer");
-};
-
 const NCAPP = "https://app.newconnection.xyz/";
 
 const AppButton = () => {
     return (
-        <button
-            onClick={() => openInNewTab(NCAPP)}
-            className="btns-rounded py-4 px-96 flex rounded-full items-center"
+        <a
+            href={NCAPP}
+            className="btns-rounded flex flex-row justify-center w-full py-4 mx-2 px-16 border-2 items-center rounded-full"
         >
             <span>Enter App</span>
-            <ExternalLinkIcon />
-        </button>
+            <div>
+                <ExternalLinkIcon />
+            </div>
+        </a>
     );
 };
 
@@ -33,13 +31,13 @@ export const MainContent = () => {
                 <p className="xl:text-8xl lg:text-5xl md:text-4xl sm:text-2xl">
                     MULTICHAIN DAO
                     <br />
-                    SOLUTION TO CREATE
+                    <div className="pl-10 lg:pl-0 lg:mt-6">SOLUTION TO CREATE</div>
                     <br />
-                    <div className="flex justify-end lg:justify-center md:justify-center sm:justify-center text-purple">
+                    <div className="flex justify-end -mt-4 lg:-mt-10 lg:justify-center md:justify-center sm:justify-center text-purple">
                         <TextAnimation />
                     </div>
                 </p>
-                <div className="flex justify-center">
+                <div className="flex justify-center bg-cover">
                     <img src={MainImg} />
                 </div>
                 <div className="flex justify-center">
