9dc5785a34f89b3831276bf7a41dba27cead2c38
Vakurin <vakurin.maxim@gmail.com>
2022-08-13 02:32:38 +0300

    close button

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,6 @@
 // NPM Dependencies
-import * as React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 // OWN Componets
 import "styles/components/Layout/Layout.module.css";
@@ -39,7 +40,7 @@ const Header = () => {
     };
 
     return (
-        <header className="header px-4 lg:px-6 py-2.5 ">
+        <header id={"header-id"} className="header px-4 lg:px-6 py-2.5 ">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <span className="self-center leading-6 text-lg font-semibold whitespace-nowrap">
                     new
@@ -84,37 +85,42 @@ const Header = () => {
                     <button
                         data-collapse-toggle="mobile-menu-2"
                         type="button"
-                        className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none
+                        className="inline-flex items-center p-1 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none
                     focus:ring-2 focus:ring-btnHover"
                         aria-controls="mobile-menu-2"
                         aria-expanded="false"
                         onClick={() => setNavbarOpen(!navbarOpen)}
                     >
-                        <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
-                        <svg
-                            className="hidden w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
+                        {navbarOpen ? (
+                            <svg
+                                className="w-6 h-5 transition-opacity delay-75"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        ) : (
+                            <>
+                                <span className="sr-only">Open main menu</span>
+                                <svg
+                                    className="w-6 h-6 transition-opacity delay-75"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </>
+                        )}
                     </button>
                 </div>
                 <MobileMenu />
