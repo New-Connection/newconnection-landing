b073e7635647a0821ffe310023f45120cf6d683b
Vakurin <vakurin.maxim@gmail.com>
2022-08-13 02:33:09 +0300

    fix styles in accordion

--- src/components/AccordionCards/AccordionCards.tsx
+++ src/components/AccordionCards/AccordionCards.tsx
@@ -38,17 +38,16 @@ export default function AccordionCards() {
                     "-mt-2 border-2 pt-4 pl-8 pb-5 border-b-0 font-medium bg-white border-gray rounded-t-xl text-purple hover:bg-purple hover:text-white hover:border-purple active:bg-btnActive active:text-purple opacity-100",
                     className
                 )}
-                activeClassName="-mt-2 bg-white border-2 text-black pt-4 px-6 lg:px-8 font-medium pb-4 mb-2 border-purple rounded-xl text-white"
+                activeClassName="-mt-2 bg-white border-2 text-black pt-4 px-6 lg:px-8 font-medium pb-4 mb-2 border-purple rounded-xl"
                 uuid={uuid}
             >
                 <AccordionItemHeading>
-                    <AccordionItemButton className="text-purple">{nameTitle}</AccordionItemButton>
+                    <AccordionItemButton>{nameTitle}</AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <div className="lg:flex">
                         <p className="text-sm text pt-4 mr-2 pb-8 font-normal lg:w-1/3 text-black">
-                            {description}. Choose what the NFTs you can give out to people for
-                            logging into the DAO will look like by creating them on our platform
+                            {description}
                         </p>
                         <div className="lg:flex lg:justify-center lg:w-2/3 flex flex-row justify-center">
                             <img src={NFTCard} className="h-72 w-54"></img>
@@ -58,7 +57,7 @@ export default function AccordionCards() {
                             ></img>
                         </div>
                     </div>
-                    <AppButton className="bg-white border-purple text-purple mt-3 pr max-h-[3.0rem] px-6" />
+                    <AppButton className="bg-white border-purple text-purple mt-3 max-h-[3.0rem] px-4" />
                 </AccordionItemPanel>
             </AccordionItem>
         );
@@ -73,24 +72,24 @@ export default function AccordionCards() {
                 className="z-10"
             />
             <AccordionItems
-                nameTitle="Propos and Vote"
-                description={TEXT_DESCRIPTION[1]}
-                className="z-20"
-            />
-            <AccordionItems
                 nameTitle="Create DAO"
                 description={TEXT_DESCRIPTION[2]}
                 className="z-30"
             />
             <AccordionItems
-                nameTitle="Transfer NFT"
-                description={TEXT_DESCRIPTION[3]}
-                className="z-40"
+                nameTitle="Propos and Vote"
+                description={TEXT_DESCRIPTION[1]}
+                className="z-20"
             />
             <AccordionItems
                 nameTitle="Create NFT"
                 description={TEXT_DESCRIPTION[4]}
                 className="border-b-2 rounded-xl z-50"
+            />
+            <AccordionItems
+                nameTitle="Transfer NFT"
+                description={TEXT_DESCRIPTION[3]}
+                className="z-40"
             />
         </Accordion>
     );
