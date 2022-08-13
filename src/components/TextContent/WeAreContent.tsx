c8adc318a2c14a5dc6af2988a4239220782b715f
Vakurin <vakurin.maxim@gmail.com>
2022-08-13 03:28:25 +0300

    rewrite texts

--- src/components/TextContent/WeAreContent.tsx
+++ src/components/TextContent/WeAreContent.tsx
@@ -8,10 +8,10 @@ export const WeAreSection = () => {
     return (
         <section className="relative w-full xl:flex lg:flex px-2">
             <div className="lg:flex space-y-2 justify-between">
-                <p className="w-1/4 text-sm text-purple">We are</p>
+                <p className="w-1/4 text-xl text-purple">We present</p>
                 <p className="w-2/3 text-lg">
-                    New Connection suggest you a convenient and easy way to create your own
-                    multichain DAO — free organization creation, NFT release, voting and fundraising
+                    a convenient and easy way to create your own multichain DAO. It can be a charity
+                    organization, NFT launch, democracy voting, or fundraising.
                 </p>
             </div>
         </section>
