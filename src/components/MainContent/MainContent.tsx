7b068fccfda7560bf1dd5ea3278f714aa7620c50
Vakurin <vakurin.maxim@gmail.com>
2022-10-07 21:59:32 +0300

    change main content

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import TextAnimation from "./TextAnimation";
-import MainImg from "assets/main-img.png";
+import MainImg from "assets/main-screen.png";
 import { AppButton } from "components/Button/AppButton";
 import { gsap } from "gsap";
 
@@ -15,48 +15,39 @@ export const MainContent = () => {
     const imageRef = useRef<HTMLDivElement>(null);
     const buttonRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        gsap.fromTo(
-            boxTextRef.current!,
-            { x: -100, opacity: 0 },
-            { x: 0, opacity: 1, delay: 0.5, duration: 1.75 }
-        );
+    // useEffect(() => {
+    //     gsap.fromTo(
+    //         boxTextRef.current!,
+    //         { x: -100, opacity: 0 },
+    //         { x: 0, opacity: 1, delay: 0.5, duration: 1.75 }
+    //     );
 
-        gsap.fromTo(
-            imageRef.current!,
-            { x: 100, opacity: 0 },
-            { x: 0, opacity: 1, delay: 0.75, duration: 1.75 }
-        );
+    //     gsap.fromTo(
+    //         imageRef.current!,
+    //         { x: 100, opacity: 0 },
+    //         { x: 0, opacity: 1, delay: 0.75, duration: 1.75 }
+    //     );
 
-        gsap.fromTo(
-            buttonRef.current!,
-            { y: 100, opacity: 0 },
-            { y: 0, opacity: 1, delay: 1.0, duration: 1.75 }
-        );
-    });
+    //     gsap.fromTo(
+    //         buttonRef.current!,
+    //         { y: 100, opacity: 0 },
+    //         { y: 0, opacity: 1, delay: 1.0, duration: 1.75 }
+    //     );
+    // });
 
     return (
-        <section className="relative w-full">
-            <div className="items-center space-y-12">
-                <div
-                    className="xl:text-8xl lg:text-5xl md:text-5xl sm:text-5xl text-4xl px-2 opacity-0 text-black"
-                    ref={boxTextRef}
-                >
-                    MULTICHAIN DAO
-                    <br />
-                    <div className="lg:pl-16">SOLUTION TO CREATE</div>
-                    <br />
-                    <div className="flex px-1 justify-start lg:ml-10 -mt-10 lg:-mt-24 lg:justify-center md:justify-start sm:justify-start text-purple">
-                        <TextAnimation />
-                    </div>
-                </div>
-                <div className="flex justify-center bg-cover" ref={imageRef}>
-                    <img src={MainImg} className="rounded-md" />
-                </div>
-                <div className="flex justify-center" ref={buttonRef}>
-                    <AppButton className="sm:py-1 " />
-                </div>
+        <div
+            className="relative text-center bg-fixed w-full max-h-screen max-w-none mb-10"
+            ref={imageRef}
+        >
+            <img src={MainImg} className="rounded-md w-full object-cover" />
+            <div className="absolute top-[12%] left-[30%] text-7xl  text-black" ref={boxTextRef}>
+                The easiest way
+                <br />
+                to create your own
+                <br />
+                <p className="italic">community</p>
             </div>
-        </section>
+        </div>
     );
 };
