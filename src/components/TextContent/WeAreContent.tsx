1f45a692bac105acc3e2a3917c56859e3d0deb92
Vakurin <vakurin.maxim@gmail.com>
2022-10-09 01:27:10 +0300

    main text for different screens

--- src/components/TextContent/WeAreContent.tsx
+++ src/components/TextContent/WeAreContent.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+// sm: "480px",
+// md: "768px",
+// lg: "976px",
+// xl: "1440px",
 export const WeAreSection = () => {
     return (
-        <section className="w-full lg:px-32 lg:my-8 md:h-[150vh] border-2 max-h-screen lg:h-[200vh] text-center">
-            <p className="text-xl xl:text-8xl lg:text-[5.25rem] lg:leading-none md:text-3xl">
+        <section className="w-full max-h-screen px-8 lg:h-[200vh] text-center">
+            <p className="text-3xl sm:text-6xl xl:text-8xl lg:text-[4.95rem] lg:leading-none md:text-4xl">
                 We present a convenient and easy way to create your own multichain DAO. It can be a
                 charity organization, NFT launch, democracy voting or fundraising
             </p>
