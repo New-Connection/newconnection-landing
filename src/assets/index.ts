82726c5cdc53aa31e689d48116b25ca4ac5e68a8
Vakurin <vakurin.maxim@gmail.com>
2022-10-08 16:46:55 +0300

    indexing images in one file

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+// social networks
+import createDAOImage from "assets/2-CreateDAO.png";
+import proposalImage from "assets/3-Proposal.png";
+import transferNFTImage from "assets/5-TransferNFT.png";
+
+const ASSETS = {
+    // social networks
+    createDAOImage: createDAOImage,
+    proposalImage: proposalImage,
+    transferNFTImage: transferNFTImage,
+    
+};
+
+export default ASSETS;
