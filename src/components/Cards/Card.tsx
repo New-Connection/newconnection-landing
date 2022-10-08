5b31caf93d940b2af8c2d9af0740b50b0b466930
Vakurin <vakurin.maxim@gmail.com>
2022-10-09 00:50:51 +0300

    added motion animation

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import classNames from "classnames";
+import { motion } from "framer-motion";
 
 export const Card = ({ className, cardObject }) => {
     return (
-        <div className={classNames("bg-gray rounded-md", className)}>
+        <motion.div
+            className={classNames("bg-gray rounded-md cursor-pointer", className)}
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.35 }}
+        >
             <div className="flex flex-1 flex-col justify-between px-6 pt-2 pb-6 h-32">
                 <div className="flex-1 mt-1">
                     <div className="flex justify-between">
@@ -17,6 +22,6 @@ export const Card = ({ className, cardObject }) => {
             <div className="flex-shrink-0">
                 <img className="h-72 w-full p-4 object-cover" src={cardObject.image} alt="" />
             </div>
-        </div>
+        </motion.div>
     );
 };
