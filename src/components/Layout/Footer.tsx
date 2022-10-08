ce312a9613bc709a21dd36dfc66d41faaf6491b5
Vakurin <vakurin.maxim@gmail.com>
2022-10-08 16:47:15 +0300

    new footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,40 +1,14 @@
 import * as React from "react";
-import { Separator } from "ariakit/separator";
-
-import { TwitterIcon } from "components/Icons/TwitterIcon";
-import { DiscordIcon } from "components/Icons/DiscordIcon";
 
 const Footer = () => {
     return (
-        <footer className="absolute bottom-0 w-full h-16 sm:h-24">
-            <Separator
-                orientation="horizontal"
-                className="mt-16 mb-4 h-0 w-full border-t-2 border-purple"
-            />
-            <div className="max-w-screen-xl pb-8 px-4 mx-auto sm:px-6 lg:px-8">
-                <div className="sm:flex sm:items-center sm:justify-between sm:space-y-2">
-                    <div className="flex justify-center sm:justify-start ">
-                        <p className="text-sm">contact@newconnection.xyz</p>
-                    </div>
-
-                    <div className="mt-4 text-sm text-center items-center text-gray-400 lg:text-center lg:mt-0 space-x-4">
-                        <button className="cursor-not-allowed hover:text-gray2">Terms</button>
-                        <button className="cursor-not-allowed hover:text-gray2">Privacy</button>
-                    </div>
-                    <div className="flex justify-center sm:justify-start space-x-2 mt-4">
-                        <button className="flex pl-[0.375em] items-center cursor-not-allowed w-[60px] h-[60px] bg-gray rounded-md fill-purple hover:fill-gray2">
-                            <DiscordIcon />
-                        </button>
-                        <a
-                            href="https://twitter.com/NewConnectionX"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <button className="w-[60px] h-[60px] pl-[0.375em] bg-gray rounded-md fill-[#1DA1F2] hover:bg-purple hover:fill-white">
-                                <TwitterIcon />
-                            </button>
-                        </a>
-                    </div>
+        <footer className="absolute bottom-0 w-full h-44 sm:h-24 px-2 pb-2">
+            <div className="max-w-full px-4 mx-auto sm:px-6 lg:px-8 bg-purple pb-1 rounded-md text-center ">
+                <p className="w-full text-8xl text-white py-16">NEW CONNECTION</p>
+                <div className="sm:flex sm:items-center sm:justify-between sm:space-y-2 text-sm text-white mb-6">
+                    <p>Terms and Conditions</p>
+                    <p className="mr-16">contact@newconnection.xyz</p>
+                    <p>Privacy Policy</p>
                 </div>
             </div>
         </footer>
