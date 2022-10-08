dd266c37c713e78f5d55f0e5fb427ea5aff757d3
Vakurin <vakurin.maxim@gmail.com>
2022-10-08 16:47:37 +0300

    present text

--- src/components/TextContent/WeAreContent.tsx
+++ src/components/TextContent/WeAreContent.tsx
@@ -2,14 +2,11 @@ import React, { useRef, useEffect } from "react";
 
 export const WeAreSection = () => {
     return (
-        <section className="relative w-full xl:flex lg:flex px-2">
-            <div className="lg:flex space-y-2 justify-between">
-                <p className="w-1/4 text-xl text-purple">We present</p>
-                <p className="w-2/3 text-lg">
-                    a convenient and easy way to create your own multichain DAO. It can be a charity
-                    organization, NFT launch, democracy voting, or fundraising.
-                </p>
-            </div>
+        <section className="w-full px-32 h-100 my-20 text-center">
+            <p className="text-8xl ">
+                We present a convenient and easy way to create your own multichain DAO. It can be a
+                charity organization, NFT launch, democracy voting or fundraising
+            </p>
         </section>
     );
 };
