0512cd57bad9f1c6077938984a9c475f9278eb99
Vakurin <vakurin.maxim@gmail.com>
2022-10-09 15:57:21 +0300

    fix image in card and responsive design

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -12,15 +12,19 @@ export const Card = ({ className, cardObject }) => {
             <div className="flex flex-1 flex-col justify-between px-6 pt-2 pb-6 h-32">
                 <div className="flex-1 mt-1">
                     <div className="flex justify-between">
-                        <p className="text-3xl font-medium text-purple  mt-1">{cardObject.title}</p>
+                        <p className="text-xl lg:text-3xl font-medium text-purple mt-1">
+                            {cardObject.title}
+                        </p>
                     </div>
                 </div>
                 <div className="mt-[0.75rem] flex items-center justify-between">
-                    <p className="text-base font-normal text-black">{cardObject.description}</p>
+                    <p className="text-sm lg:text-base font-normal text-black">
+                        {cardObject.description}
+                    </p>
                 </div>
             </div>
-            <div className="flex-shrink-0">
-                <img className="h-72 w-full p-4 object-cover" src={cardObject.image} alt="" />
+            <div className="flex-shrink-0 flex justify-center content-center">
+                <img className="h-56 lg:h-72 p-4 object-cover" src={cardObject.image} alt="" />
             </div>
         </motion.div>
     );
