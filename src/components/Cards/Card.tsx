585fc86e9f931625dff121e1595c38e31bfbdd11
Vakurin <vakurin.maxim@gmail.com>
2022-10-09 17:27:57 +0300

    change hover animation on tap

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -6,7 +6,7 @@ export const Card = ({ className, cardObject }) => {
     return (
         <motion.div
             className={classNames("bg-gray rounded-md cards", className)}
-            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 1.05 }}
             transition={{ duration: 0.35 }}
         >
             <div className="flex flex-1 flex-col justify-between px-6 pt-2 pb-6 h-32">
